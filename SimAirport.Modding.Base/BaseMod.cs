7d6d92ee460800d5290d9ab8f3f069a85bb38ed7
Joscha Germerott <joscha.germerott@googlemail.com>
2019-04-09 23:45:31 +0200

    Add project files.

---
+++ SimAirport.Modding.Base/BaseMod.cs
@@ -0,0 +1,26 @@
+﻿namespace SimAirport.Modding.Base
+{
+	public abstract class BaseMod {
+		//Basic information
+		/// <summary>
+		/// Name of your mod.
+		/// </summary>
+		public abstract string Name { get; }
+
+		/// <summary>
+		/// Description of your mod.
+		/// </summary>
+		public abstract string Description { get; }
+
+		/// <summary>
+		/// You.
+		/// </summary>
+		public abstract string Author { get; }
+
+		/// <summary>
+		/// Called once per frame, use to update data that must be updated each fram.
+		/// Use wisely as heavy load slows down the game!
+		/// </summary>
+		public abstract void OnTick();
+	}
+}
---
+++ SimAirport.Modding.Base/Data/GameTime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimAirport.Modding.Base.Data {
+	/// <summary>
+	/// Gives access to GameTime
+	/// </summary>
+	public class GameTime {
+		public static GameTime Instance { get; private set; }
+
+		public GameTime(Func<double> internalTotalGameSeconds) {
+			//Only one instance can be created, it'll be created by the game.
+			if (Instance != null)
+				return;
+
+			Instance = this;
+
+			InternalTotalGameSeconds = internalTotalGameSeconds;
+		}
+
+
+		/// <summary>
+		/// Returns the total time this game has been running in in game seconds
+		/// </summary>
+		public double TotalGameSeconds => InternalTotalGameSeconds.Invoke();
+		private readonly Func<double> InternalTotalGameSeconds;
+	}
+}
