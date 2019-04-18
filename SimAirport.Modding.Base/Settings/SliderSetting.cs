61e2ddf39f94babf9b60e9ee9bab70b32ea982c7
Joscha Germerott <joscha.germerott@googlemail.com>
2019-04-18 23:25:50 +0200

    Added JsonIgnore to onValueChanged

--- SimAirport.Modding.Base/Settings/SliderSetting.cs
+++ SimAirport.Modding.Base/Settings/SliderSetting.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,6 +27,7 @@ namespace SimAirport.Modding.Base.Settings {
 			}
 		}
 
+		[JsonIgnore]
 		public override Action<float> OnValueChanged { get; set; }
 	}
 }
--- SimAirport.Modding.Base/Settings/StringSetting.cs
+++ SimAirport.Modding.Base/Settings/StringSetting.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,7 @@ namespace SimAirport.Modding.Base.Settings {
 			}
 		}
 
+		[JsonIgnore]
 		public override Action<string> OnValueChanged { get; set; }
 	}
 }
