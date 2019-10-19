0475dab568eff3620fd73a241ac787664360d586
Joscha Germerott <joscha.germerott@googlemail.com>
2019-10-19 11:40:00 +0200

    Added money balance to Game

--- SimAirport.Modding.Base/Data/Game.cs
+++ SimAirport.Modding.Base/Data/Game.cs
@@ -8,16 +8,26 @@ namespace SimAirport.Modding.Data {
 	public class Game {
 		public static Game Instance { get; private set; }
 
-		public Game(Func<GameState> internalGetState) {
+		public Game(Func<GameState> internalGetState, Func<int> internalGetMoneyBalance, Action<int> internalSetMoneyBalance) {
 			if (Instance != null)
 				throw new InvalidOperationException("Tried to create 2nd " + GetType().Name);
 
 			Instance = this;
 
 			InternalGetState = internalGetState;
+			InternalGetMoneyBalance = internalGetMoneyBalance;
+			InternalSetMoneyBalance = internalSetMoneyBalance;
 		}
 
 		public GameState GetState() => InternalGetState.Invoke();
 		private readonly Func<GameState> InternalGetState;
+
+		public int MoneyBalance {
+			get => InternalGetMoneyBalance.Invoke();
+			set => InternalSetMoneyBalance.Invoke(value);
+		}
+
+		private readonly Func<int> InternalGetMoneyBalance;
+		private readonly Action<int> InternalSetMoneyBalance;
 	}
 }
