91a220d148839747d019741a1de4b0ad6291bc7a
Joscha Germerott <joscha.germerott@googlemail.com>
2020-02-09 21:31:44 +0100

    Added label setting to display text in mod settings

---
+++ SimAirport.Modding.Base/Settings/LabelSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimAirport.Modding.Settings {
+	public class LabelSetting : SettingBase<string> {
+		public override string Name { get; set; }
+
+		[Obsolete("Label setting does not have a value, only a Name!")]
+		public override string Value { get; set; }
+
+		[Obsolete("Label settings value never changes!")]
+		public override Action<string> OnValueChanged { get; set; }
+	}
+}
