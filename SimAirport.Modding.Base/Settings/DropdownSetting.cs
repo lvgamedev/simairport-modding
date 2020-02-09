4342994f19b3d8deddedf1698c5e362a9ea5e001
Joscha Germerott <joscha.germerott@googlemail.com>
2020-02-09 21:16:49 +0100

    Added dropdown mod setting

---
+++ SimAirport.Modding.Base/Settings/DropdownSetting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimAirport.Modding.Settings {
+	public class DropdownSetting : SettingBase<int> {
+		public override string Name { get; set; }
+
+		private int _value;
+
+		/// <summary>
+		/// Index of the selected option in the Dropdown.
+		/// </summary>
+		public override int Value {
+			get => _value;
+			set {
+				if (value != _value) {
+					OnValueChanged?.Invoke(value);
+					_value = value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// I18n keys for your selectable options.
+		/// </summary>
+		public List<string> Options { get; set; }
+
+		public override Action<int> OnValueChanged { get; set; }
+	}
+}
