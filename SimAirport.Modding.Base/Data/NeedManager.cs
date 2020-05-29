dd88f3d327468cf736cde45894f97ced1e818e6e
BR146 <65818738+BR146@users.noreply.github.com>
2020-05-29 22:42:02 +0200

    Need & Zone API
    Added NeedManager API.
    Added ZoneManager API.

---
+++ SimAirport.Modding.Base/Data/NeedManager.cs
@@ -0,0 +1,45 @@
+﻿using Needs;
+using System;
+
+namespace SimAirport.Modding.Data {
+    public class NeedManager {
+
+        public static NeedManager Instance { get; private set; }
+
+
+        /// <summary>
+        /// Creates singleton of this manager.
+        /// </summary>
+        /// <param name="internalRegisterNeed">NeedManagerInternal.InternalRegisterNeed</param>
+        /// <param name="internalUnregisterNeed">NeedManagerInternal.InternalUnregisterNeed</param>
+        public NeedManager(Action<Type> internalRegisterNeed, Action<Type> internalUnregisterNeed) {
+            if( Instance != null )
+                throw new InvalidOperationException("Tried to create 2nd " + GetType().Name);
+
+            Instance = this;
+
+            InternalRegisterNeed = internalRegisterNeed;
+            InternalUnregisterNeed = internalUnregisterNeed;
+        }
+
+
+        /// <summary>
+        /// Register a new need to the game.
+        /// </summary>
+        /// <typeparam name="T">The class defining the new need.</typeparam>
+        public void RegisterNeed<T>() where T : Need, new() {
+            InternalRegisterNeed(typeof(T));
+        }
+        private readonly Action<Type> InternalRegisterNeed;
+
+
+        /// <summary>
+        /// Unregisters the given need.
+        /// </summary>
+        /// <typeparam name="T">The class defining the new need.</typeparam>
+        public void UnregisterNeed<T>() where T : Need {
+            InternalUnregisterNeed(typeof(T));
+        }
+        private readonly Action<Type> InternalUnregisterNeed;
+    }
+}
---
+++ SimAirport.Modding.Base/Data/ZoneManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TerrainTools;
+
+namespace SimAirport.Modding.Data {
+    public class ZoneManager {
+
+        public static ZoneManager Instance { get; private set; }
+
+
+        /// <summary>
+        /// Creates singleton of this manager.
+        /// </summary>
+        /// <param name="internalRegisterZone">ZoneManagerInternal.InternalRegisterZone</param>
+        /// <param name="internalUnregisterZone">ZoneManagerInternal.InternalUnregisterZone</param>
+        public ZoneManager(Func<Type, string, ZoneToolConfig, ZoneTool> internalRegisterZone, Action<ZoneTool> internalUnregisterZone) {
+            if( Instance != null )
+                throw new InvalidOperationException("Tried to create 2nd " + GetType().Name);
+
+            Instance = this;
+
+            InternalRegisterZone = internalRegisterZone;
+            InternalUnregisterZone = internalUnregisterZone;
+        }
+
+
+        /// <summary>
+        /// Registers a new zone.
+        /// </summary>
+        /// <typeparam name="T">Zone Derivative with zone's logic</typeparam>
+        /// <param name="config"></param>
+        /// <param name="uniqueId">globally unique zone identifier</param>
+        /// <param name="i18nNameKey">zone name shown in menu and as overlay</param>
+        /// <param name="i18nDescKey">description of the zone in the build menu</param>
+        /// <returns>Zone Tool instance representing the new build tool</returns>
+        public ZoneTool RegisterZone<T>(string uniqueId, ZoneToolConfig config) where T : Zone {
+            return InternalRegisterZone(typeof(T), uniqueId, config);
+        }
+        private readonly Func<Type, string, ZoneToolConfig, ZoneTool> InternalRegisterZone;
+
+
+        /// <summary>
+        /// Remove a zone tool.
+        /// </summary>
+        /// <param name="which">The zone tool to be unregistered from the game.</param>
+        public void UnregisterZone(ZoneTool which) {
+            InternalUnregisterZone(which);
+        }
+        private readonly Action<ZoneTool> InternalUnregisterZone;
+
+    }
+}
